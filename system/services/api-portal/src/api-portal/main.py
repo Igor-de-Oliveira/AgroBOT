d36dabd0063b58112c36c4e9d90be995e054af8a
Igor de Oliveira da Silva <igor.03deoliveira@gmail.com>
2026-02-20 15:00:29 -0300

    Portal agrobot
    Inicio da criação de uma pagina para orquestração de toda a aplicação
    
    essa api terá como intuito juntar todas as funções em um único lugar e tirar a necessidade do usuário de acessar um swagger para utilizar as funções do Bot
    
    Como também posteriormente essa funcionalidade permitira o uso de filas no projeto como tbm a utilização do mesmo como SaaS para múltiplos usuários

---
+++ system/services/api-portal/src/api-portal/main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+import uvicorn
+import os
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+dirname = os.path.dirname(__file__)
+templates = Jinja2Templates(directory=os.path.join(dirname, "templates"))
+
+@app.get("/", response_class=HTMLResponse)
+async def tela(request: Request):
+    return templates.TemplateResponse("main.html", {"request": request})
+
+app.mount("/static", StaticFiles(directory=os.path.join(dirname, "static")), name="static")
+
+
+@app.get("/process_ods", response_class=HTMLResponse)
+async def process_ods(request: Request):
+    return templates.TemplateResponse("procesamento_arquivos.html", {"request": request})
+
+def main():
+    """Inicia a API utilizando Uvicorn."""
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+if __name__ == "__main__":
+    main()
